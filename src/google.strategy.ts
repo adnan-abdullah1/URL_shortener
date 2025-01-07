ca9f687654c1b079094aba7d750bab0c7b2def1a
adnan-abdullah <adnan.abdullah@lelafe.com>
2025-01-08 00:47:15 +0530

    add validate to google strategy back

--- src/google.strategy.ts
+++ src/google.strategy.ts
@@ -16,4 +16,21 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       scope: ['email', 'profile'],
     });
   }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { name, emails, photos } = profile;
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+      picture: photos[0].value,
+      accessToken,
+    };
+    done(null, user);
+  }
 }
